989f2188b21a3379284d2cb8f31ddf96c6632243
Leonilla Forsblom <leonilla.forsblom@mindler.se>
2020-12-11 20:26:43 +0100

    Changed stuff

--- 1.10/stringReader/index.js
+++ 1.10/stringReader/index.js
@@ -7,15 +7,22 @@ const fs = require('fs')
 const dirPath = path.join(__dirname, '../files');
 const filePath = path.join(dirPath, 'timeStamps.txt')
 
+let responseData = 'This will be data'
+
 const readTimeStampFile = async () => {
     await fs.readFile(filePath, (error, data) => {
         if (error) {
           console.error('Error reading file', error)
           return
         }
+        responseData = data
         console.log(data)
     })
 }
+
+app.get('/', (req, res) => {
+  res.send(responseData);
+})
 
 app.listen(port, () => {
     readTimeStampFile()
