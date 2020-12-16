79a837970f55abfc11f290bc0eec4a4e64eaef60
Leonilla Forsblom <leonilla.forsblom@mindler.se>
2020-12-16 21:32:55 +0100

    Took out unnecassary stuff

--- 1.12/projectApplication/index.js
+++ 1.12/projectApplication/index.js
@@ -15,17 +15,6 @@ app.use(dirPath, express.static(dirPath))
 const filePath = path.join(dirPath, 'pongs.txt')
 const imagePath = path.join(dirPath, 'image.jpg')
 
-const stringGenerator = () => {
-  const length = 10
-  let result = ''
-  const characters= 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-  const charactersLength = characters.length
-  for ( let i = 0; i < length; i++ ) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength))
-  }
-  return result
-}
-
 const ImageAlreadyExists = async () => new Promise(res => {
   fs.stat(imagePath, (err, stats) => {
     if (err || !stats) return res(false)
@@ -40,38 +29,12 @@ const getImage = async () => {
     console.log('finished downloading!'));
 }
 
-const string = stringGenerator()
-let response = `${new Date()}: ${string}`
-
-const getStringWithDate = async () => {
-  setInterval(() => {
-    timeStamp = new Date()
-    response = `${timeStamp}: ${string}\n`
-  }, 5000)
-  return response
-}
-
-const readPongFile = async () => new Promise(res => {
-  fs.readFile(filePath, (err, buffer) => {
-    if (err) return console.log('FAILED TO READ FILE', '----------------', err)
-    res(buffer)
-  })
-})
-
-
-const getStringWPongs = async () => {
-  const string = await getStringWithDate()
-  const pongs = await readPongFile()
-  return `${string} Ping / Pongs: ${pongs}`
-}
-
 app.get('/', async (req, res) => {
   const imageExists = await ImageAlreadyExists()
   if (!imageExists) {
     await getImage()
   }
-  const data = await getStringWPongs()
-  res.render("index", { pongs: data, imagePath })
+  res.render("index", { imagePath })
 })
 
 app.listen(port, () => {
